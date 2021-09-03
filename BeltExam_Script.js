c439eb6306cbf1f7bd349e460d1a9377060ccabb
Luke Hufnagle <leh17068@gmail.com>
2021-09-03 12:39:01 -0400

    Mock Website template

---
+++ BeltExam_Script.js
@@ -0,0 +1,11 @@
+function remove(){
+    document.querySelector(".cookiePop").remove()
+}
+
+function changeImage(){
+    document.getElementById("mainAloePic").src = "assets/succulents-2.jpg"
+}
+
+function changeImage2(){
+    document.getElementById("mainAloePic").src = "assets/succulents-1.jpg"
+}
